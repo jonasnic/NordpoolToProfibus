1be92e0840f2aad0597b2ced3b26a657b1e66ee8
Jonas Nicolaysen <jonas.nicolaysen@no.abb.com>
2022-08-25 11:08:16 +0200

    Createt a simple Webserver that return current prices

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,11 @@ EthernetClient client;
 NordPool NP;
 #define UPDATEPRICEINTERVALL 15000
 
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer Wserver(80);
+
 void setup()
 {
     // Open serial communications and wait for port to open:
@@ -35,6 +40,65 @@ void loop()
         updateTimer = millis();
         NP.update();
         NP.print();
+    }
+
+    // listen for incoming clients
+    client = Wserver.available();
+    if (client)
+    {
+        Serial.println("new client");
+        // an http request ends with a blank line
+        boolean currentLineIsBlank = true;
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                char c = client.read();
+                Serial.write(c);
+                // if you've gotten to the end of the line (received a newline
+                // character) and the line is blank, the http request has ended,
+                // so you can send a reply
+                if (c == '\n' && currentLineIsBlank)
+                {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close"); // the connection will be closed after completion of the response
+                    client.println("Refresh: 5");        // refresh the page automatically every 5 sec
+                    client.println();
+                    client.println("<!DOCTYPE HTML>");
+                    client.println("<html>");
+
+                    // output the value of each analog input pin
+                    for (int clock = 0; clock < 25; clock++)
+                    {
+
+                        float price = NP.getPrice(clock);
+                        client.print(clock);
+                        client.print(": \t");
+                        client.print(price);
+                        client.println("<br />");
+                    }
+                    client.println("</html>");
+                    break;
+                }
+                if (c == '\n')
+                {
+                    // you're starting a new line
+                    currentLineIsBlank = true;
+                }
+                else if (c != '\r')
+                {
+                    // you've gotten a character on the current line
+                    currentLineIsBlank = false;
+                }
+            }
+        }
+        // give the web browser time to receive the data
+        delay(1);
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
     }
 
     ////////////////////
